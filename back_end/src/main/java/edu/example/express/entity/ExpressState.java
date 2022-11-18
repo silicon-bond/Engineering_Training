04cdb60994fa4fea8eeb1457a95839265a69f6a2
silicon-bond <79505663+silicon-bond@users.noreply.github.com>
2022-11-18 10:39:08 +0800

    增加物流状态类

---
+++ back_end/src/main/java/edu/example/express/entity/ExpressState.java
@@ -0,0 +1,17 @@
+package edu.example.express.entity;
+
+public class ExpressState {
+
+    public static final int Uncollected = 0;
+
+    public static final int Collected = 1;
+
+    public static final int InTransit = 2;
+
+    public static final int ToBeDelivered = 3;
+
+    public static final int Delivering = 4;
+
+    public static final int Delivered = 5;
+
+}
